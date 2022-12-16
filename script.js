330234e28f6d038732747c48c0d8905fd7baa83c
Rixen <Rixenlama@gmail.com>
2022-12-16 06:23:18 +0200

    Resonsive Nav Bar, Still needs some transition probably.

---
+++ script.js
@@ -0,0 +1,13 @@
+function openNav(){
+    document.getElementById("menu-nav").style.display = "flex";
+    document.getElementById("exit-menu").style.display = "block";
+    document.getElementById("menu").style.display = "none";
+}
+
+
+
+function closeNav(){
+    document.getElementById("menu-nav").style.display = "none";
+    document.getElementById("exit-menu").style.display = "none";
+    document.getElementById("menu").style.display = "block";
+}
