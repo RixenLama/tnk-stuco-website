8cb529e632840d82244d6af13b639c71f5db12ec
Rixen <Rixenlama@gmail.com>
2022-12-20 21:22:24 +0200

    Scrolling bar disabled when moblie nav is open.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function openNav(){
     document.getElementById("menu-nav").style.display = "flex";
     document.getElementById("exit-menu").style.display = "block";
     document.getElementById("menu").style.display = "none";
+    document.getElementsByTagName("body")[0].style.overflow = "hidden";
 }
 
 
@@ -10,4 +11,5 @@ function closeNav(){
     document.getElementById("menu-nav").style.display = "none";
     document.getElementById("exit-menu").style.display = "none";
     document.getElementById("menu").style.display = "block";
+    document.getElementsByTagName("body")[0].style.overflow = "auto";
 }
