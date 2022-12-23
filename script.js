5cecd9ea599e9a0603d9f734ee9db4ba77397203
Rixen <Rixenlama@gmail.com>
2022-12-23 08:22:10 +0200

    Dynamic Favicon, changes depending on the browser display.

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ function closeNav() {
   document.getElementById("menu").style.display = "block";
   document.getElementsByTagName("body")[0].dataset.open = "false";
 }
+
+$(document).ready(function() {
+  if (!window.matchMedia)
+      return;
+
+  var current = $('head > link[rel="icon"][media]');
+  $.each(current, function(i, icon) {
+      var match = window.matchMedia(icon.media);
+      function swap() {
+          if (match.matches) {
+              current.remove();
+              current = $(icon).appendTo('head');
+          }
+      }
+      match.addListener(swap);
+      swap();
+  });
+});
