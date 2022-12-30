a88ec3ad40fbcdfedb1464c64fb5754be1123312
Raiyan Shamim <razash139@gmail.com>
2022-12-30 13:06:06 +0200

    add dynamic delay

--- js/quotes.js
+++ js/quotes.js
@@ -1,13 +1,25 @@
 const quoteCardContainer = document.querySelector("#quote-card-container");
 const quoteCardTemplate = document.querySelector("#quote-card-template");
 
+const minQuotesize = 280;
+const gridGap = 16;
+const containerWidth = quoteCardContainer.clientWidth;
+const columnNumber = Math.floor(
+  (containerWidth + gridGap) / (minQuotesize + gridGap)
+);
+
 fetch("/data/quotes.json")
   .then((res) => res.json())
   .then((quotes) => {
-    quotes.forEach((quote) => {
+    quotes.forEach((quote, idx) => {
       const { content, cite } = quote;
 
+      const delay = (idx % columnNumber) * 50;
+
       const quoteClone = quoteCardTemplate.content.cloneNode(true);
+      quoteClone
+        .querySelector("#quote-card")
+        .setAttribute("data-aos-delay", delay);
       quoteClone.querySelector("#quote-content").innerText = content;
       quoteClone.querySelector(
         "#quote-cite"
